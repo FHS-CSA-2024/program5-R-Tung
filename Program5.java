b35ea0621ee7c94827b378a13e1282629eb9e91e
R-Tung <107238461+R-Tung@users.noreply.github.com>
2024-08-15 12:31:57 -0700

    Update Program5.java
    started prok 5

--- Program5.java
+++ Program5.java
@@ -1,7 +1,24 @@
 //import stuff here?
 
 //Your code here
-
+public class Program5{
+    public static void main(String[] args){
+        int royaleMiles = 286;
+        int royaleGallons = 9;
+        
+        int koopaKingMiles = 412;
+        int koopaKingGallons = 40;
+        
+        int pipeFrameMiles = 361;
+        int pipeFrameGallons = 18;
+        
+        int badWagonMiles = 161;
+        int badWagonGallons = 11;
+        
+        
+        
+    }
+}
 //Paste console output below:
 /*
 
