c3e404ab09c84b66727b8167a4537e1e43f68983
R-Tung <107238461+R-Tung@users.noreply.github.com>
2024-08-19 14:24:45 -0700

    Update Program5.java
    basically done, need to round to 1 decimal place

--- Program5.java
+++ Program5.java
@@ -1,5 +1,5 @@
 //import stuff here?
-
+import java.text.DecimalFormat;
 //Your code here
 public class Program5{
     public static void main(String[] args){
@@ -15,12 +15,24 @@ public class Program5{
         int badWagonMiles = 161;
         int badWagonGallons = 11;
         
+        double royaleAvg = 0;
+        double koopaKingAvg = 0;
+        double pipeFrameAvg = 0;
+        double badWagonAvg = 0;
+
+        royaleAvg = (double)royaleMiles / royaleGallons;
+        koopaKingAvg = (double)koopaKingMiles / koopaKingGallons;
+        pipeFrameAvg = (double)pipeFrameMiles / pipeFrameGallons;
+        badWagonAvg = (double)badWagonMiles / badWagonGallons;
+            
+        
+        System.out.println("Mushroom Cup Prix Racer Average Miles/Per Gallon");
+        System.out.println("Royale averaged: " + (royaleAvg));
+        System.out.println("Koopa King averaged: " + koopaKingAvg);
+        System.out.println("Pipe Frame avereraged: " + pipeFrameAvg);
+        System.out.println("Badwagon averaged: " + badWagonAvg);
+        
         
         
     }
 }
-//Paste console output below:
-/*
-
-
-*/
